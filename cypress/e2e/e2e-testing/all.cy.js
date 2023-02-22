d694f790ef5ace103f2297a81441be89eeeeca58
Andrei Selvanovich <andreis@aaisonline.com>
2023-02-22 14:57:20 -0600

    Update AWS CodeBuild config

---
+++ cypress/e2e/e2e-testing/all.cy.js
@@ -0,0 +1,78 @@
+//browse
+import '../browse/browseProduct1StateLossCostCurrent.cy'
+import '../browse/browseResults.cy'
+import '../browse/browseResultsVariousCards.cy'
+import '../browse/browseScreenFilters.cy'
+import '../browse/imgBrowseResults.cy'
+import '../browse/imgBrowseScreenFilters.cy'
+
+//data
+import '../data/buckets/bucketPage.cy'
+import '../data/buckets/statusChecks.cy'
+import '../data/bucketsPage.cy'
+
+//e2e-testing
+import './filter/filter.feature'
+import './leftSideMenu/leftSideMenu.feature'
+import './login/login.feature'
+import './productMaterial/productMaterial.feature'
+
+//home
+import '../home/browseAllProducts.cy'
+import '../home/myRecentProducts.cy'
+import '../home/riskAwarenessMaps.cy'
+import '../home/search.cy'
+import '../home/statisticalReporting.cy'
+
+//lines
+import '../lines/productLine/checkState.cy'
+import '../lines/productLine/downloadMaterials.cy'
+import '../lines/productLine/productCertificationStatus.cy'
+import '../lines/productLine/validateLine.cy'
+
+//notifications
+import '../notifications/notifications.cy'
+
+//orgs
+import '../orgs/org1/adminView/addresses/checkAddresses.cy'
+import '../orgs/org1/adminView/contacts/addContacts.cy'
+import '../orgs/org1/adminView/contacts/removeContacts.cy'
+import '../orgs/org1/adminView/history/checkAddressesHistroy.cy'
+import '../orgs/org1/adminView/history/checkContactsHistory.cy'
+import '../orgs/org1/adminView/history/checkUsersHistory.cy'
+import '../orgs/org1/adminView/users/addUser.cy'
+import '../orgs/org1/adminView/users/changeRole.cy'
+import '../orgs/org1/adminView/users/deleteUser.cy'
+import '../orgs/org1/userView/loginProcess.cy'
+import '../orgs/org1/userView/orgScreen.cy'
+import '../orgs/org1/userView/registrationProcess.cy'
+import '../orgs/CheckAvatarForEmployee.cy'
+import '../orgs/CheckAvatarForNonemployee.cy'
+import '../orgs/checkFooter.cy'
+import '../orgs/checkHeader.cy'
+import '../orgs/createOrg.cy'
+import '../orgs/orgsScreen.cy'
+
+//profile
+import '../profile/checkInfo.cy'
+import '../profile/selectNotificationPreferences.cy'
+
+//risk screen
+import '../risk screen/riskScreenRiskAwarenessMaps.cy'
+
+//smoke
+import '../smoke/loginLogout.cy'
+import '../smoke/userSnap.cy'
+
+//utils
+import '../utils/livraria.cy'
+
+
+
+
+
+
+
+
+
+
