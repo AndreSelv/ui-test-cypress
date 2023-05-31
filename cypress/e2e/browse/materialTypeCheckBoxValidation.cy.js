ef649ebf69d75b34dc326b58b29bbbf4444b33ab
Andrei Selvanovich <andreis@aaisonline.com>
2023-05-31 15:49:01 -0500

    add check box func for All contents

--- cypress/e2e/browse/materialTypeCheckBoxValidation.cy.js
+++ cypress/e2e/browse/materialTypeCheckBoxValidation.cy.js
@@ -4,15 +4,12 @@ describe("US109198 Validate subcategories in material type section", () => {
     cy.initAmplify();
     cy.login();
     cy.visit("#/browse");
-    // cy.get(".MuiFormControlLabel-root").click();
     cy.get("#packageType-select").click();
   });
 
   it("Validate ALL Content check box functionality", () => {
-
     cy.get(`input[type="checkbox"]`)
       .as("checkboxes").check(MATERIALS[0][0], { force: true });
-
     for (let i = 0; i < MATERIALS.length; i++)
       cy.wrap(MATERIALS[i]).each((type) => {
         cy.get(`input[value="${type}"]`).should("be.checked").and("have.value", type);
@@ -22,14 +19,30 @@ describe("US109198 Validate subcategories in material type section", () => {
   it("Validate Each check box functionality", () => {
 
     for (let i = 1; i < MATERIALS.length; i++) {
-
       cy.get(`input[type="checkbox"]`)
         .as("checkboxes").check([MATERIALS[i][0]], { force: true });
-
       cy.wrap(MATERIALS[i]).each((type) => {
         cy.get(`input[value="${type}"]`).should("be.checked").and("have.value", type);
       });
     }
   });
+
+
+  it("Validate each material category should reflected after it was selected", () => {
+    for (let i = 1; i < MATERIALS.length; i++) {
+      cy.get(`input[type="checkbox"]`)
+        .as("checkboxes").check(MATERIALS[i][0], { force: true });
+      cy.get(`[data-test="packageType"]`).should('contain.text', MATERIALS[i][0])
+      cy.get(`@checkboxes`).uncheck(MATERIALS[i][0], { force: true })
+    }
+  });
+
+  it("Should reflected ellipsis if selected more then one material type", () => {
+      cy.get(`input[type="checkbox"]`)
+        .as("checkboxes").check([MATERIALS[1][0], MATERIALS[2][0]], { force: true });
+      cy.get(`[data-test="packageType"]`).should('contain.text', `...`)
+  });
+
+
 
 });
