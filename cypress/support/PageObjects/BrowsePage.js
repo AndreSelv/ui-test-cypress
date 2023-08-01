6db29b9c58b87b6cb08a870dd146efe67578b4b8
Andrei Selvanovich <andreis@aaisonline.com>
2023-08-01 18:44:11 -0500

    remove dev and uat from CICD config

--- cypress/support/PageObjects/BrowsePage.js
+++ cypress/support/PageObjects/BrowsePage.js
@@ -4,16 +4,16 @@ class BrowsePage {
     return cy.get("#product-select");
   }
 
-  getCloseProductsButton(){
-    return cy.xpath(`(//*[@data-testid="CloseIcon"])[1]`)
+  getCloseProductsButton() {
+    return cy.xpath(`(//*[@data-testid="CloseIcon"])[1]`);
   }
 
-  getProductSection(){
-    return cy.get(`div[data-test="browseProduct"]`)
+  getProductSection() {
+    return cy.get(`div[data-test="browseProduct"]`);
   }
 
-  getBrowseCountResultField(){
-    return cy.xpath("//h6/../p")
+  getBrowseCountResultField() {
+    return cy.xpath("//h6/../p");
   }
 
   getMaterial() {
@@ -44,13 +44,39 @@ class BrowsePage {
     return cy.get(`span:contains("more")`);
   }
 
-  getListOfPublicationsCards(){
-    return cy.xpath(`//div[contains(@class, "infinite-scroll-component ")]//p`)
+  getListOfPublicationsCards() {
+    return cy.xpath(`//div[contains(@class, "infinite-scroll-component ")]//p`);
   }
 
   selectProduct(product) {
     return this.getProduct().type(`${product} - {downArrow}{enter}{esc}`);
   }
+
+  getBrowseResultLineBadge(number = 0) {
+    return cy.get(`[data-test=browseResults-item-${number}-line-badge]`);
+  }
+
+  getBrowseResultLineIcon(number = 0) {
+    return cy.get(`[data-test=browseResults-item-${number}-line-icon]`);
+  }
+
+  getBrowseResultLineTooltip(number = 0) {
+    return cy.get(`[data-test=browseResults-item-${number}-line-tooltip]`);
+  }
+
+
+  getBrowseResultStateBadge(number = 0) {
+    return cy.get(`[data-test=browseResults-item-${number}-state-badge]`);
+  }
+
+  getBrowseResultStateIcon(number = 0) {
+    return cy.get(`[data-test=browseResults-item-${number}-state-icon]`);
+  }
+
+  getBrowseResultStateTooltip(number = 0) {
+    return cy.get(`[data-test=browseResults-item-${number}-state-tooltip]`);
+  }
+
 
   selectMaterialType(type) {
     this.getMaterial().click();
