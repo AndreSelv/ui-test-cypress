18a8c5a572aeaf1f612ae37b418f9ae3171cedc7
Andrei Selvanovich <andreis@aaisonline.com>
2023-11-13 11:31:20 -0600

    trimming

--- cypress/e2e/home/search.cy.js
+++ cypress/e2e/home/search.cy.js
@@ -13,6 +13,9 @@ describe("Search Functionality", () => {
 
           cy.viewport(size, orientation);
         });
+        const noSpase = "CL0811";
+        const spase = "CL 0811";
+        const trim = " CL 0811 ";
         it("enter a product in the search box and vailidate information returns", () => {
           // cy.intercept("POST", "/assets/v1/search", { fixture: "browse/browseResultsVariousCards.json" });
           cy.visit("/#");
@@ -23,6 +26,72 @@ describe("Search Functionality", () => {
           browsePage.selectMaterialType("Forms");
           browsePage.getListOfPublicationsCards().each(($el) => {
             expect($el.text()).contains("Liability");
+          });
+        });
+
+        it("US113741 AAISdirect | Support search box searches space usage (feature parity) from home page", () => {
+          cy.visit("/#");
+          homePage.typeSearchBar(spase);
+          cy.wait(1000);
+          cy.url().should("contain", "/#/browse");
+          browsePage.getExcludeFileContentCheckBox().click();
+          browsePage.publicationsShouldBeEqual(2);
+          browsePage.getSearchField().clear();
+          browsePage.typeSearch(noSpase);
+          browsePage.publicationsShouldBeEqual(2);
+          browsePage.getListOfPublicationsCards().each(($el) => {
+            expect($el.text()).contains(spase);
+          });
+        });
+
+        it("US113741 AAISdirect | Support search box searches space usage (feature parity) ", () => {
+          cy.visit("/#");
+          homePage.typeSearchBar(noSpase);
+          cy.wait(1000);
+          cy.url().should("contain", "/#/browse");
+          browsePage.getExcludeFileContentCheckBox().click();
+          browsePage.publicationsShouldBeEqual(2);
+          browsePage.getSearchField().clear();
+          browsePage.typeSearch(spase);
+          browsePage.publicationsShouldBeEqual(2);
+          browsePage.getListOfPublicationsCards().each(($el) => {
+            expect($el.text()).contains(spase);
+          });
+        });
+
+        it("US113741 AAISdirect | Support search box searches space usage (feature parity) from browser page", () => {
+          cy.visit("/#/browse");
+          browsePage.selectProduct("AGGL");
+          browsePage.selectMaterialType("Forms", "Advisory Information");
+          browsePage.getExcludeFileContentCheckBox().click();
+          browsePage.typeSearch(spase);
+          browsePage.selectState("AK");
+          browsePage.getListOfPublicationsCards().each(($el) => {
+            expect($el.text()).contains(spase);
+          });
+        });
+
+        it("US113741 AAISdirect | Support search box searches space usage (feature parity)", () => {
+          cy.visit("/#/browse");
+          browsePage.selectProduct("AGGL");
+          browsePage.selectMaterialType("Forms", "Advisory Information");
+          browsePage.getExcludeFileContentCheckBox().click();
+          browsePage.typeSearch(noSpase);
+          browsePage.selectState("AK");
+          browsePage.getListOfPublicationsCards().each(($el) => {
+            expect($el.text()).contains(spase);
+          });
+
+        });
+        it("US113741 AAISdirect | Support search box searches space usage (feature parity) Trim leading and trailing spaces", () => {
+          cy.visit("/#");
+          homePage.typeSearchBar(trim);
+          cy.wait(1000);
+          cy.url().should("contain", "/#/browse");
+          browsePage.getExcludeFileContentCheckBox().click();
+          browsePage.publicationsShouldBeEqual(2);
+          browsePage.getListOfPublicationsCards().each(($el) => {
+            expect($el.text()).contains(spase);
           });
         });
       });
