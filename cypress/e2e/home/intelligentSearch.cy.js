6fd9ff31561867ad8151e9b03abb94daaa3eb272
Andrei Selvanovich <andreis@aaisonline.com>
2023-11-13 15:23:14 -0600

    trimming

---
+++ cypress/e2e/home/intelligentSearch.cy.js
@@ -0,0 +1,41 @@
+const HomePage = require("../../support/PageObjects/HomePage");
+const BrowsePage = require("../../support/PageObjects/BrowsePage");
+const homePage = new HomePage();
+const browsePage = new BrowsePage();
+const TESTS = require("../../fixtures/enums/TESTS");
+describe("Search Functionality", () => {
+  Cypress.env("SIZES").forEach((size) => {
+    Cypress.env("ORIENTATION").forEach((orientation) => {
+      describe(`Device: ${size}, Orientation: ${orientation}`, () => {
+        beforeEach(() => {
+          cy.initAmplify();
+          cy.login();
+
+          cy.viewport(size, orientation);
+        });
+
+        it("US114519 Accommodate form and bulletin searches more intelligently", () => {
+
+          cy.wrap(TESTS).each((test) => {
+            cy.visit("/#");
+            homePage.typeSearchBar(test.case);
+            cy.wait(700);
+            cy.url().should("contain", "/#/browse");
+            browsePage.getExcludeFileContentCheckBox().click();
+            browsePage.publicationsShouldBeEqual(test.result);
+            browsePage.getListOfPublicationsCards().each(($el) => {
+              expect($el.text()).contains(test.expect);
+            });
+
+            browsePage.getSearchField().clear();
+            browsePage.typeSearch(test.caseQuotes);
+            browsePage.publicationsShouldBeEqual(test.result);
+            browsePage.getListOfPublicationsCards().each(($el) => {
+              expect($el.text()).contains(test.expect);
+            });
+          });
+        });
+      });
+    });
+  });
+});
---
+++ cypress/fixtures/enums/TESTS.js
@@ -0,0 +1,67 @@
+/**
+ * @description The enumeration of the Lines
+ * @since 1.0.0
+ * @enum {String}
+ * @readonly
+ * @public
+ */
+
+const TESTS = [
+  {
+    case: "UM 0811",
+    caseQuotes: "\"UM 0811\"",
+    result: 7,
+    expect: "UM 0811",
+  },
+  {
+    case: "UM0811",
+    caseQuotes: "\"UM0811\"",
+    result: 7,
+    expect: "UM 0811",
+
+  },
+  {
+    case: "CL 0136",
+    caseQuotes: "\"CL 0136\"",
+    result: 5,
+    expect: "CL 0136",
+  },
+  {
+    case: "CL0136",
+    caseQuotes: "\"CL0136\"",
+    result: 5,
+    expect: "CL 0136",
+  },
+  {
+    case: "AG 0130 06 05",
+    caseQuotes: "\"AG 0130 06 05\"",
+    result: 1,
+    expect: "AG 0130 06 05",
+  },
+  {
+    case: "BP-351",
+    caseQuotes: "\"BP-351\"",
+    result: 6,
+    expect: "351",
+  },
+  {
+    case: "BP 351",
+    caseQuotes: "\"BP 351\"",
+    result: 6,
+    expect: "351",
+  },
+  {
+    case: "BP351",
+    caseQuotes: "\"BP351\"",
+    result: 6,
+    expect: "351",
+  },
+  {
+    case: "23-0641",
+    caseQuotes: "\"23-0641\"",
+    result: 1,
+    expect: "23-0641",
+  }
+];
+
+export default TESTS;
