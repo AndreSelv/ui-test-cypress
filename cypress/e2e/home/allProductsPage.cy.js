e54255c22864f08f2d6d3ab7848c30215d502827
Andrei Selvanovich <andreis@aaisonline.com>
2024-01-18 08:27:56 -0600

    ALL products lines page testing

--- cypress/e2e/home/allProductsPage.cy.js
+++ cypress/e2e/home/allProductsPage.cy.js
@@ -11,8 +11,16 @@ describe("Validate all functionality on the ALL products line pages", () => {
   });
   it("US118446 All product lines page validation", () => {
     cy.url().should("include", "/#/lines/All");
-    cy.contains("Download Latest All Program Materials")
-    cy.contains("My Recent All Downloads")
-    cy.contains("Download Latest All Files List (.csv)")
+    cy.contains("Download Latest All Program Materials");
+    cy.contains("My Recent All Downloads");
+    cy.contains("Download Latest All Files List (.csv)");
+    cy.contains("Program Materials");
+    cy.contains("Recent Changes");
+  });
+  it("US118446 All product lines page validation", () => {
+    cy.url().should("include", "/#/lines/All");
+    cy.contains("Recent Changes").should("be.visible").click();
+    cy.contains("Program Materials").should("be.visible").click();
+    cy.contains("Download Latest All Program Materials");
   });
 });
