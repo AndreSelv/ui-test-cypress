7752efd81c3f9327edf5fe22933fb6182f1e01f7
Andrei Selvanovich <andreis@aaisonline.com>
2024-02-15 10:52:01 -0600

    US120178 Verify that screen elements on "Downloads" screen hided

--- cypress/e2e/lines/productLine/validateLine.cy.js
+++ cypress/e2e/lines/productLine/validateLine.cy.js
@@ -12,12 +12,21 @@ describe("Validate Product Line", () => {
 
         it("Product Line Description is shown correctly", () => {
           cy.visit("#/lines/BOP");
-          linesPage.getExpendMoreButton().click()
+          linesPage.getExpendMoreButton().click();
           linesPage.getLineDescription().contains(/./);
         });
         it("Product Line Title is shown correctly", () => {
           cy.visit("#/lines/BOP");
           linesPage.getTitleBar().contains(/./);
+        });
+
+        it(`US120178 Verify that screen elements on "Downloads" screen hided`, (product = "HO") => {
+          cy.visit(`#/lines/${product}`);
+          linesPage.getTitleBar().contains(/./);
+          cy.contains("Recent Changes").should("not.exist");
+          cy.contains(`Download Latest ${product} Program Materials`).should("not.exist");
+          cy.contains(`Download Latest ${product} Files List (.csv)`).should("not.exist");
+          cy.contains(`My Recent ${product} Downloads`).should("be.visible");
         });
       });
     });
