f6bf3f49ca612daca3a1a79e7887f207bd0f90b1
Andrei Selvanovich <andreis@aaisonline.com>
2024-02-26 12:22:02 -0600

    DE2604

--- cypress/e2e/profile/checkInfo.cy.js
+++ cypress/e2e/profile/checkInfo.cy.js
@@ -1,129 +1,107 @@
+const UserPage = require("../../support/PageObjects/UserPage");
+const userPage = new UserPage();
 describe("Access My Profile - Update Info", () => {
-  Cypress.env("SIZES").forEach((size) => {
-    Cypress.env("ORIENTATION").forEach((orientation) => {
-      describe(`Device: ${size}, Orientation: ${orientation}`, () => {
-        beforeEach(() => {
-          cy.initAmplify();
-          cy.login();
-          cy.visit("#/");
-          cy.contains("TU").click();
-          cy.contains("My Profile").click();
-          cy.viewport(size, orientation);
-        });
 
-        it("Update First Name", () => {
+  beforeEach(() => {
+    cy.initAmplify();
+    cy.login();
+    cy.visit("#/");
+    cy.contains("TU").click();
+    cy.contains("My Profile").click();
+  });
 
-          cy.get("[data-test=titlebar]").should("contain", "Test");
-          // Delete "hn" from "John" and adding "ey"
-
-          cy.get("[data-test=firstName]").clear().type("Teey");
-
-          cy.get('.MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root').click();
-
-          cy.get("[data-test=titlebar]").should("contain", "Teey");
-
-          cy.get("[data-test=firstName]").clear().type("Test");
-
-          cy.get('.MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root').click();
-
-          cy.get("[data-test=titlebar]").should("contain", "Test");
-
-        });
-
-        it("Update Last Name", () => {
-          cy.get("[data-test=titlebar]").should("contain", "Test User");
-
-          cy.get("[data-test=lastName]").type(
-            "{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}Selvan"
-          );
-
-          cy.get('.MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root').click();
-          cy.get("[data-test=titlebar]").should("contain", "Selvan");
-          cy.get("[data-test=lastName]").clear().type('User')
-          cy.get('.MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root').click();
-          cy.get("[data-test=titlebar]").should("contain", "User");
-
-        });
-
-        it("Update Phone", () => {
-          // cy.fixture("orgs/org1/myUser").then((user) => {
-          //   user.phoneNumbers = {
-          //     office: [
-          //       {
-          //         phoneNumber: "2171234567",
-          //         phoneNumberId: "phone1",
-          //         countryCode: "+1",
-          //         created: "2020-07-13T20:29:03.334Z",
-          //       },
-          //     ],
-          //   };
-          //   cy.route(
-          //     "GET",
-          //     "/users/659a5e72-01c2-41dc-b962-e797b25d1636",
-          //     user
-          //   );
-          // });
-
-          cy.get("[data-test=Phone]").clear().type("2171234567");
-          cy.get('.MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root').click();
-          cy.get("[data-test=Phone]").should("have.value", "2171234567");
-          cy.get("[data-test=Phone]").clear().type("123456789");
-          cy.get('.MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root').click();
-          cy.get("[data-test=Phone]").should("have.value", "123456789");
-
-        });
-
-        it("Update Phone and Extension", () => {
-
-          cy.get("[data-test=Phone]").clear().type("2171234567");
-          cy.get("[data-test=Extension]").clear().type("1234");
-
-          cy.get('.MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root').click();
-
-          cy.get('[data-test=Phone]')
-            .should("have.value", "2171234567");
-          cy.get("[data-test=Phone]").clear().type("123456789");
-
-          cy.get('.MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root').click();
-
-          cy.get('[data-test=Phone]')
-            .should("have.value", "123456789");
-        });
-
-        it("First name Is Empty", () => {
-          cy.get("[data-test=snackbar]").should("not.exist");
-
-          cy.get("[data-test=firstName]").type("{selectall}{backspace}");
-          cy.get('.MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root').click();
-
-          cy.get("[data-test=snackbar]")
-            .should("be.visible")
-            .and("contain", "Firstname cannot be empty");
-        });
-
-        it("Last name Is Empty", () => {
-          cy.get("[data-test=snackbar]").should("not.exist");
-
-          cy.get("[data-test=lastName]").type("{selectall}{backspace}");
-          cy.get('.MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root').click();
-
-          cy.get("[data-test=snackbar]")
-            .should("be.visible")
-            .and("contain", "Lastname cannot be empty");
-        });
-
-        it("Phone is Empty", () => {
-          cy.get("[data-test=snackbar]").should("not.exist");
-
-          cy.get("[data-test=Phone]").type("{selectall}{backspace}");
-          cy.get("[data-test=Extension]").type("1234");
-          cy.get('.MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root').click();
-
-          cy.get("[data-test=snackbar]")
-            .should("be.visible")
-            .and("contain", "Phone cannot be empty");
-        });
-      });
+  it("Update First Name", () => {
+    userPage.getUserFullName().then(($fn) => {
+      let testName = "Teey";
+      let initName = $fn.text().substring(0, $fn.text().indexOf(" "));
+      userPage.getTitleBar().should("contain", initName);
+      // Delete "hn" from "John" and adding "ey"
+      userPage.getFirstNameField().clear().type(testName);
+      userPage.getUpdateButton().click();
+      userPage.getTitleBar().should("contain", testName);
+      userPage.getFirstNameField().clear().type(initName);
+      userPage.getUpdateButton().click();
+      userPage.getTitleBar().should("contain", initName);
     });
+  });
+
+  it("Update Last Name", () => {
+    userPage.getUserFullName().then(($ln) => {
+      let testName = "Selvan";
+      let initName = $ln.text().substring($ln.text().indexOf(" ") + 1);
+      userPage.getTitleBar().should("contain", initName);
+      userPage.getLastNameField().clear().type(testName);
+      userPage.getUpdateButton().click();
+      userPage.getTitleBar().should("contain", testName);
+      userPage.getLastNameField().clear().type(initName);
+      userPage.getUpdateButton().click();
+      userPage.getTitleBar().should("contain", initName);
+    });
+  });
+
+  it("Update Phone", () => {
+    // cy.fixture("orgs/org1/myUser").then((user) => {
+    //   user.phoneNumbers = {
+    //     office: [
+    //       {
+    //         phoneNumber: "2171234567",
+    //         phoneNumberId: "phone1",
+    //         countryCode: "+1",
+    //         created: "2020-07-13T20:29:03.334Z",
+    //       },
+    //     ],
+    //   };
+    //   cy.route(
+    //     "GET",
+    //     "/users/659a5e72-01c2-41dc-b962-e797b25d1636",
+    //     user
+    //   );
+    // });
+    let testPhone = `2171234567`;
+    let initPhone = `2323232323`;
+    userPage.getPhoneField().clear().type(testPhone);
+    userPage.getUpdateButton().click();
+    userPage.getPhoneField().should("have.value", testPhone);
+    userPage.getPhoneField().clear().type(initPhone);
+    userPage.getUpdateButton().click();
+    userPage.getPhoneField().should("have.value", initPhone);
+  });
+
+  it("Update Phone and Extension", () => {
+    userPage.getPhoneField().clear().type("2171234567");
+    userPage.getExtensionField().clear().type("1234");
+    userPage.getUpdateButton().click();
+    userPage.getPhoneField().should("have.value", "2171234567");
+    userPage.getPhoneField().clear().type("123456789");
+    userPage.getUpdateButton().click();
+    userPage.getPhoneField().should("have.value", "123456789");
+  });
+
+  it("First name Is Empty", () => {
+    userPage.getSnackBar().should("not.exist");
+    userPage.getFirstNameField().type("{selectall}{backspace}");
+    userPage.getUpdateButton().click();
+    userPage.getSnackBar()
+      .should("be.visible")
+      .and("contain", "Firstname cannot be empty");
+  });
+
+  it("Last name Is Empty", () => {
+    userPage.getSnackBar().should("not.exist");
+    userPage.getLastNameField().type("{selectall}{backspace}");
+    userPage.getUpdateButton().click();
+    userPage.getSnackBar()
+      .should("be.visible")
+      .and("contain", "Lastname cannot be empty");
+  });
+
+  it("Phone is Empty", () => {
+    userPage.getSnackBar().should("not.exist");
+    userPage.getPhoneField().type("{selectall}{backspace}");
+    userPage.getExtensionField().type("1234");
+    userPage.getUpdateButton().click();
+    userPage.getSnackBar()
+      .should("be.visible")
+      .and("contain", "Phone cannot be empty");
   });
 });
--- cypress/support/PageObjects/UserPage.js
+++ cypress/support/PageObjects/UserPage.js
@@ -19,6 +19,38 @@ class UserPage {
       .should("equal", parseInt($value.text().match(/\(([^)]+)\)/)[1])));
   }
 
+  getTitleBar() {
+    return cy.get("[data-test=titlebar]");
+  }
+
+  getFirstNameField() {
+    return cy.get("[data-test=firstName]");
+  }
+
+  getLastNameField() {
+    return cy.get("[data-test=lastName]");
+  }
+
+  getUpdateButton() {
+    return cy.get(".MuiGrid-justify-content-xs-flex-end > :nth-child(2) > .MuiButtonBase-root");
+  }
+
+  getUserFullName(){
+    return cy.xpath(`(//*[@data-test="list-item-text"])[7]/span`)
+  }
+
+  getPhoneField(){
+    return cy.get("[data-test=Phone]")
+  }
+
+  getExtensionField(){
+    return cy.get("[data-test=Extension]")
+  }
+
+  getSnackBar(){
+    return cy.get("[data-test=snackbar]")
+  }
+
 }
 
 export default UserPage;
