32e29b911709d48e829208697f8a2b5f77b9a89f
Andrei Selvanovich <andreis@aaisonline.com>
2024-06-07 12:03:57 -0500

    US123493 Product page - Commercial | Testing

--- cypress/support/PageObjects/ProductPage.js
+++ cypress/support/PageObjects/ProductPage.js
@@ -4,28 +4,28 @@ class ProductPage {
     return cy.contains("Products");
   }
 
-  getProductAuto(){
-    return cy.get("#menuItem-auto")
+  getProductAuto() {
+    return cy.get("#menuItem-auto");
   }
 
-  getProductCommercial(){
-    return cy.get("#menuItem-commercial")
+  getProductCommercial() {
+    return cy.get("#menuItem-commercial");
   }
 
-   getProductFarm(){
-    return cy.get("#menuItem-farmag")
+  getProductFarm() {
+    return cy.get("#menuItem-farmag");
   }
 
-   getProductInlandMarine(){
-    return cy.get("#menuItem-inlandmarine")
+  getProductInlandMarine() {
+    return cy.get("#menuItem-inlandmarine");
   }
 
-   getProductPersonal(){
-    return cy.get("#menuItem-personal")
+  getProductPersonal() {
+    return cy.get("#menuItem-personal");
   }
 
-  getLandingPageBar(){
-    return cy.get('#landing-page-bar-undefined')
+  getLandingPageBar() {
+    return cy.get("#landing-page-bar-undefined");
   }
 
   getCommercialAutoTab() {
@@ -84,4 +84,65 @@ class ProductPage {
       this.getCardAnnouncements(), this.getCardBulletins(), this.getCardRecentlyPublished(),
       this.getCardManualMaterials(), this.getCardForms(), this.getCardAdditionalMaterials()];
   }
-}export default ProductPage;
+
+  getArtisansTab() {
+    return cy.get("#product-button-Artisans");
+  }
+
+  getBusinessownersTab() {
+    return cy.get("#product-button-Businessowners");
+  }
+
+  getCannabisBusinessownersTab() {
+    return cy.get("[name=\"product-button-Cannabis Businessowners\"]");
+  }
+
+  getCombinationPolicyProgramTab() {
+    return cy.get("[name=\"product-button-Combination Policy Program\"]");
+  }
+
+  getCommercialCrimeTab() {
+    return cy.get("[name=\"product-button-Commercial Crime\"]");
+  }
+
+  getCommercialInlandMarineFiledTab() {
+    return cy.get("[name=\"product-button-Commercial Inland Marine (Filed)\"]");
+  }
+
+  getCommercialLiabilityTraditionalTab() {
+    return cy.get("[name=\"product-button-Commercial Liability (Traditional)\"]");
+  }
+
+  getCommercialLiabilityCLTTab() {
+    return cy.get("[name=\"product-button-Commercial Liability (w/ CLT)\"]");
+  }
+
+  getCommercialOutputProgramTab() {
+    return cy.get("[name=\"product-button-Commercial Output Program\"]");
+  }
+
+  getCommercialPropertiesTab() {
+    return cy.get("[name=\"product-button-Commercial Properties\"]");
+  }
+
+  getCommercialUmbrellaTab() {
+    return cy.get("[name=\"product-button-Commercial Umbrella\"]");
+  }
+
+  getGlassTab() {
+    return cy.get("#product-button-Glass");
+  }
+
+
+  getListOfProducts() {
+    return cy.xpath("//div[@id=\"product-grid-undefined\"]//button");
+  }
+
+  getListOfMenuProductsCategory() {
+    return cy.xpath("//ul[@id=\"menuItemsList-productsUL\"]//a");
+  }
+
+
+}
+
+export default ProductPage;
